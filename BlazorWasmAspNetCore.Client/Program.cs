7bbf7e0b95948d1723819e5f6de7f43730997abf
Andrew Herrick <aj@andrewherrick.com>
2025-02-24 08:53:48 -0500

    Initial commit

---
+++ BlazorWasmAspNetCore.Client/Program.cs
@@ -0,0 +1,15 @@
+using BlazorApp_empty1.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+    Timeout = TimeSpan.FromMinutes(5),
+});
+
+await builder.Build().RunAsync();
